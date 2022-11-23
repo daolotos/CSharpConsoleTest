49f3abffcfbc0b1ca3975c9fad6df55fd21d10ca
daolotos <117892786+daolotos@users.noreply.github.com>
2022-11-23 15:09:44 +0300

    Сделана Задача 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 ﻿using System;
 
-Console.WriteLine("Введите число: ");
-string numberString = Console.ReadLine();
-int x = Int32.Parse(numberString);
-Console.WriteLine("квадрат числа " + x + " равен: " + x * x); 
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+
+Console.Write("max = ");
+Console.Write(max);
+
+
+
