c032977357725f8cafd949d29140887412e6bfa8
daolotos <117892786+daolotos@users.noreply.github.com>
2022-11-25 20:12:14 +0300

    Задание сделала

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= N; i = i + 2)
+{
+    Console.Write(i);
+    Console.Write("  ");
+}
+
