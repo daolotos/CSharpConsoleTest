ee4276486c2ec01c41084dbfaccc3041344f5c3b
daolotos <117892786+daolotos@users.noreply.github.com>
2022-11-25 14:11:12 +0300

    Сделала 6 задачу

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+int max;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+Console.Write("max = ");
+Console.Write(max);
+
---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+    Console.WriteLine("Да,это четное число!");
+else
+    Console.WriteLine("Это не то число, давай еще раз");
+
+
+
+
+
